f76687797ea1e737c947a82bf80ad16d4a62c123
AzureAD\박근영 <kypark@uws.co.kr>
2025-09-01 17:19:29 +0900

    comp 1000 시작

---
+++ Assignment4/Assignment4/Image/Color.cs
@@ -0,0 +1,16 @@
+﻿namespace Assignment4.Image
+{
+    public struct Color
+    {
+        public byte R { get; private set; }
+        public byte G { get; private set; }
+        public byte B { get; private set; }
+
+        public Color(byte r, byte g, byte b)
+        {
+            R = r;
+            G = g;
+            B = b;
+        }
+    }
+}
