a2550ff813022fc62d1f8c1fe5713a7f0c14f56d
AzureAD\박근영 <kypark@uws.co.kr>
2025-09-16 16:57:01 +0900

    assignment 1 start

---
+++ Assignment1/Assignment1/BigNumberCalculator.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Text;
+
+namespace Assignment1
+{
+    public class BigNumberCalculator
+    {
+        public BigNumberCalculator(int bitCount, EMode mode)
+        {
+        }
+
+        public static string GetOnesComplementOrNull(string num)
+        {
+            if (!num.StartsWith("0b"))
+            {
+                return null;
+            }
+            if (num == "0b")
+            {
+                return null;
+            }
+            for (int i = 2; i < num.Length; i++)
+            {
+                if (!(num[i] == '0' || num[i] == '1'))
+                {
+                    return null;
+                }
+            }
+
+            StringBuilder sb = new StringBuilder("0b",num.Length);
+
+            for (int i = 2; i < num.Length; i++)
+            {
+                if (num[i] == '0')
+                {
+                    sb.Append('1');
+                }
+                else
+                {
+                    sb.Append('0');
+                }
+            }
+
+            string numOnesComplement = sb.ToString();
+
+            return numOnesComplement;
+        }
+
+        public static string GetTwosComplementOrNull(string num)
+        {
+            if (!num.StartsWith("0b"))
+            {
+                return null;
+            }
+            if (num == "0b")
+            {
+                return null;
+            }
+            for (int i = 2; i < num.Length; i++)
+            {
+                if (!(num[i] == '0' || num[i] == '1'))
+                {
+                    return null;
+                }
+            }
+
+            StringBuilder sb = new StringBuilder("0b", num.Length);
+
+            for (int i = 2; i < num.Length; i++)
+            {
+                if (num[i] == '0')
+                {
+                    sb.Append('1');
+                }
+                else
+                {
+                    sb.Append('0');
+                }
+            }
+
+            int count = 0;
+            for (int i = sb.Length - 1; i > 1; i--)
+            {
+                if (sb[i] == '1')
+                {
+                    count++;
+                }
+                else
+                {
+                    sb.Replace('1', '0', i, sb.Length - 1);
+                }
+            }
+
+            string numTwosComplement = sb.ToString();
+
+            return numTwosComplement;
+        }
+
+        public static string ToBinaryOrNull(string num)
+        {
+            return null;
+        }
+
+        public static string ToDecimalOrNull(string num)
+        {
+            return null;
+        }
+
+        public static string ToHexOrNull(string num)
+        {
+            return null;
+        }
+
+        public string AddOrNull(string num1, string num2, out bool bOverflow)
+        {
+            bOverflow = false;
+            return null;
+        }
+
+        public string SubtractOrNull(string num1, string num2, out bool bOverflow)
+        {
+            bOverflow = false;
+            return null;
+        }
+    }
+}
--- Assignment1/Assignment1/Program.cs
+++ Assignment1/Assignment1/Program.cs
@@ -5,8 +5,18 @@ namespace Assignment1
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        {   
+            string num1 = "0b0100101101";
+            string num2 = "koihjowr";
+            string num3 = "0b";
+            string num4 = "0b3";
+            string num5 = "0x1";
+
+            Console.WriteLine(BigNumberCalculator.GetOnesComplementOrNull(num1));
+            Console.WriteLine(BigNumberCalculator.GetOnesComplementOrNull(num2));
+            Console.WriteLine(BigNumberCalculator.GetOnesComplementOrNull(num3));
+            Console.WriteLine(BigNumberCalculator.GetOnesComplementOrNull(num4));
+            Console.WriteLine(BigNumberCalculator.GetOnesComplementOrNull(num5));
         }
     }
 }
