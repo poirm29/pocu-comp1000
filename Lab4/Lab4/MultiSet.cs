327dca5edc539ca4a5e1f61cef142923f5df626a
AzureAD\박근영 <kypark@uws.co.kr>
2025-09-22 17:55:09 +0900

    lab 4 start

---
+++ Lab4/Lab4/MultiSet.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using System.Dynamic;
+
+namespace Lab4
+{
+    public sealed class MultiSet
+    {
+        public List<string> MultisetList { get; private set; }
+        
+        public MultiSet ()
+        {
+            List<string> multisetList = new List<string>();
+            MultisetList = multisetList;
+        }
+        public void Add(string element)
+        {
+            MultisetList.Add(element);
+            // ToDo 만약 정렬이 필요하다면 정렬함수 만들어서 호출
+        }
+
+        public bool Remove(string element)
+        {
+            int sizeOfMultisetList = MultisetList.Count;
+
+            for (int i = 0; i < sizeOfMultisetList - 1; i++)
+            {
+                if (MultisetList[i] == element)
+                {
+                    MultisetList.RemoveAt(i);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public uint GetMultiplicity(string element)
+        {
+            return 0;
+        }
+
+        public List<string> ToList()
+        {
+            return MultisetList;
+        }
+
+        public MultiSet Union(MultiSet other)
+        {
+            return null;
+        }
+
+        public MultiSet Intersect(MultiSet other)
+        {
+            return null;
+        }
+
+        public MultiSet Subtract(MultiSet other)
+        {
+            return null;
+        }
+
+        public List<MultiSet> FindPowerSet()
+        {
+            return null;
+        }
+
+        public bool IsSubsetOf(MultiSet other)
+        {
+            return false;
+        }
+
+        public bool IsSupersetOf(MultiSet other)
+        {
+            return false;
+        }
+    }
+}
--- Lab4/Lab4/Program.cs
+++ Lab4/Lab4/Program.cs
@@ -6,7 +6,22 @@ namespace Lab4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MultiSet first = new MultiSet();
+
+            string element1 = "aand";
+            string element2 = "kakaoa";
+
+            first.Add(element1);
+            first.Add(element2);
+
+            Console.WriteLine($"First element is {first.ToList()[0]}");
+            Console.WriteLine($"Second element is {first.ToList()[1]}");
+
+            first.Remove(element1);
+
+            Console.WriteLine($"After remove First element is {first.ToList()[0]}");
+
+
         }
     }
 }
