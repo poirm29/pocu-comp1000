de9aeaf4a6a00c9f84e2d6510de7ddc9b9d54df6
AzureAD\박근영 <kypark@uws.co.kr>
2025-10-22 13:19:04 +0900

    lab4 restart

--- Lab4/Lab4/MultiSet.cs
+++ Lab4/Lab4/MultiSet.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Dynamic;
 
 namespace Lab4
 {
@@ -15,14 +14,15 @@ namespace Lab4
         public void Add(string element)
         {
             MultisetList.Add(element);
-            // ToDo 만약 정렬이 필요하다면 정렬함수 만들어서 호출
+
+            return;
         }
 
         public bool Remove(string element)
         {
             int sizeOfMultisetList = MultisetList.Count;
 
-            for (int i = 0; i < sizeOfMultisetList - 1; i++)
+            for (int i = 0; i < sizeOfMultisetList; i++)
             {
                 if (MultisetList[i] == element)
                 {
@@ -35,27 +35,164 @@ namespace Lab4
 
         public uint GetMultiplicity(string element)
         {
-            return 0;
+            int sizeOfMultisetList = MultisetList.Count;
+            uint count = 0;
+            for (int i = 0; i < sizeOfMultisetList; i++)
+            {
+                if (MultisetList[i] == element)
+                {
+                    count++;
+                }
+            }
+            return count;
         }
 
         public List<string> ToList()
         {
+            MultisetList.Sort();
+
             return MultisetList;
         }
 
         public MultiSet Union(MultiSet other)
         {
-            return null;
+            MultiSet union = new MultiSet();
+            union.MultisetList = MultisetList;
+
+            List<string> othersList = new List<string>();
+            othersList = other.ToList();
+
+            string previousString;
+            string currentString;
+
+            uint othersMultiplicity = 0;
+            uint ownMultiplicity = 0;
+
+            for (int i = 0; i < othersList.Count; i++)
+            {
+                if (i == 0)
+                {
+                    othersMultiplicity = other.GetMultiplicity(othersList[i]);
+                    ownMultiplicity = GetMultiplicity(othersList[i]);
+
+                    if (othersMultiplicity > ownMultiplicity)
+                    {
+                        for (int j = 0; j < othersMultiplicity - ownMultiplicity; j++)
+                        {
+                            union.Add(othersList[i]);
+                        }
+                    }
+                }
+                else
+                {
+                    previousString = othersList[i - 1];
+                    currentString = othersList[i];
+
+                    if (previousString != currentString)
+                    {
+                        othersMultiplicity = other.GetMultiplicity(othersList[i]);
+                        ownMultiplicity = GetMultiplicity(othersList[i]);
+
+                        if (othersMultiplicity > ownMultiplicity)
+                        {
+                            for (int j = 0; j < othersMultiplicity - ownMultiplicity; j++)
+                            {
+                                union.Add(othersList[i]);
+                            }
+                        }
+                    }
+                }
+            }
+            return union;
         }
 
         public MultiSet Intersect(MultiSet other)
         {
-            return null;
+            MultiSet intersect = new MultiSet();
+
+            List<string> othersList = new List<string>();
+            othersList = other.ToList();
+
+            string previousString;
+            string currentString;
+
+            uint othersMultiplicity = 0;
+            uint ownMultiplicity = 0;
+
+            uint minMultiplicity = 0;
+
+            for (int i = 0; i < othersList.Count; i++)
+            {
+                if (i == 0)
+                {
+                    ownMultiplicity = GetMultiplicity(othersList[i]);
+                    othersMultiplicity = other.GetMultiplicity(othersList[i]);
+
+                    if (ownMultiplicity != 0 && othersMultiplicity != 0)
+                    {
+                        if (ownMultiplicity >= othersMultiplicity)
+                        {
+                            minMultiplicity = othersMultiplicity;
+                        }
+                        else
+                        {
+                            minMultiplicity = ownMultiplicity;
+                        }
+
+                        for (int j = 0; j < minMultiplicity; j++)
+                        {
+                            intersect.Add(othersList[i]);
+                        }
+                    }
+                }
+                else
+                {
+                    previousString = othersList[i - 1];
+                    currentString = othersList[i];
+
+                    if (previousString != currentString)
+                    {
+                        ownMultiplicity = GetMultiplicity(othersList[i]);
+                        othersMultiplicity = other.GetMultiplicity(othersList[i]);
+
+                        if (ownMultiplicity != 0 && othersMultiplicity != 0)
+                        {
+                            if (ownMultiplicity >= othersMultiplicity)
+                            {
+                                minMultiplicity = othersMultiplicity;
+                            }
+                            else
+                            {
+                                minMultiplicity = ownMultiplicity;
+                            }
+
+                            for (int j = 0; j < minMultiplicity; j++)
+                            {
+                                intersect.Add(othersList[i]);
+                            }
+                        }
+                    }
+                }
+            }
+
+
+            return intersect;
         }
 
         public MultiSet Subtract(MultiSet other)
         {
-            return null;
+            MultiSet subtract = new MultiSet();
+            subtract.MultisetList = MultisetList;
+
+            List<string> othersList = new List<string>();
+            othersList = other.ToList();
+
+            for (int i = 0; i < othersList.Count; i++)
+            {
+                subtract.Remove(othersList[i]);
+            }
+
+            return subtract;
         }
 
         public List<MultiSet> FindPowerSet()
@@ -71,6 +208,12 @@ namespace Lab4
         public bool IsSupersetOf(MultiSet other)
         {
             return false;
+        }
+
+        private void LexicographicOrder(List<string> MultisetList)
+        {
+
+            return;
         }
     }
 }
