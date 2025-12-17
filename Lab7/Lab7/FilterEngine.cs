57ff546674e4e552faace3e08d77dacd5a4ccbcf
AzureAD\박근영 <kypark@uws.co.kr>
2025-12-17 16:28:42 +0900

    Lab 7 Implement except last feture, GetSortKey()

---
+++ Lab7/Lab7/FilterEngine.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7
+{
+    public static class FilterEngine
+    {
+        public static List<Frame> FilterFrames(List<Frame> frames, EFeatureFlags features)
+        {
+            List<Frame> filteredFramesList = new List<Frame>();
+            
+            foreach (var frame in frames)
+            {
+                if ((frame.Features & features) != 0)
+                {
+                    filteredFramesList.Add(frame);
+                }
+            }
+
+            return filteredFramesList;
+        }
+
+        public static List<Frame> FilterOutFrames(List<Frame> frames, EFeatureFlags features)
+        {
+            List<Frame> filteredOutFramesList = new List<Frame>();
+
+            foreach (var frame in frames)
+            {
+                if ((frame.Features & features) == 0)
+                {
+                    filteredOutFramesList.Add(frame);
+                }
+            }
+
+            return filteredOutFramesList;
+        }
+
+        public static List<Frame> Intersect(List<Frame> frames1, List<Frame> frames2)
+        {
+            List<Frame> intersectionList = new List<Frame>();
+            foreach (var frameIn1 in frames1)
+            {
+                foreach (var frameIn2 in frames2)
+                {
+                    if (frameIn1 == frameIn2)
+                    {
+                        intersectionList.Add(frameIn2);
+                        break;
+                    }
+                }
+            }
+
+            return intersectionList;
+        }
+    }
+}
---
+++ Lab7/Lab7/Frame.cs
@@ -0,0 +1,35 @@
+﻿using System.Data;
+
+namespace Lab7
+{
+    public class Frame
+    {
+        public EFeatureFlags Features { get; private set; }
+        public uint ID { get; set; }
+        public string Name { get; set; }
+        public byte FeatureBoard { get; private set; }
+        public Frame(uint id, string name)
+        {
+            Features = EFeatureFlags.Default;
+            ID = id;
+            Name = name;
+            FeatureBoard = 0;
+        }
+
+        public void ToggleFeatures(EFeatureFlags features)
+        {
+            Features ^= features;
+        }
+
+        public void TurnOnFeatures(EFeatureFlags features)
+        {
+            Features |= features;
+        }
+
+        public void TurnOffFeatures(EFeatureFlags features)
+        {
+            features = ~features;
+            Features &= features;
+        }
+    }
+}
