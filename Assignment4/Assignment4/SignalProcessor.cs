ae34979ab7fec5954b74e98be7735ca3766631e3
DESKTOP-17OU9TF\kypark <kypark@uws.co.kr>
2026-07-07 17:46:58 +0900

    clear except last bitmap convol

---
+++ Assignment4/Assignment4/SignalProcessor.cs
@@ -0,0 +1,108 @@
+﻿using Assignment4.Image;
+using System;
+using System.Drawing;
+
+namespace Assignment4
+{
+    public static class SignalProcessor
+    {
+        private const double PI = 3.141592653589793;
+        private const double E = 2.718281828459045;
+        public static double[] GetGaussianFilter1D(double sigma)
+        {
+            double filterLength = sigma * 6;
+            if (filterLength % 2 < 1)
+            {
+                filterLength++;
+            }
+
+            int l = (int)filterLength;
+
+            double[] filter = new double[l];
+
+            int filterMedian = (l / 2);
+
+            for (int i = 0; i < filter.Length; i++)
+            {
+                filter[i] = GaussianCal(sigma, filterMedian - i);
+            }
+
+            return filter;
+        }
+        public static double[] Convolve1D(double[] signal, double[] filter)
+        {
+            double[] convolvedSignal = new double[signal.Length];
+
+            for (int i = 0; i < signal.Length; i++)
+            {
+                int x = filter.Length / 2;
+                for (int j = filter.Length - 1; j >= 0; j--)
+                {
+                    if (i - x >= 0 && i - x < signal.Length)
+                    {
+                        convolvedSignal[i] += signal[i - x] * filter[j];
+                    }
+                    x--;
+                }
+            }
+            return convolvedSignal;
+        }
+
+        public static double[,] GetGaussianFilter2D(double sigma)
+        {
+            double filterLength = sigma * 6;
+            if (filterLength % 2 < 1)
+            {
+                filterLength++;
+            }
+
+            double[,] filter = new double[(int)filterLength, (int)filterLength];
+
+            int filterMedian = (int)filterLength / 2;
+
+            for (int i = 0; i < filter.GetLength(0); i++)
+            {
+                for (int j = 0; j < filter.GetLength(1); j++)
+                {
+                    filter[i, j] = GaussianCal(sigma, filterMedian - i, filterMedian - j);
+                }
+            }
+
+            return filter;
+        }
+
+        public static Bitmap ConvolveImage(Bitmap bitmap, double[,] filter)
+        {
+            return null;
+        }
+
+        public static double GaussianCal(double sigma, int x)
+        {
+            if (x < 0)
+            {
+                x = -x;
+            }
+            double coefficient = 1.0 / (sigma * Math.Sqrt(2 * PI));
+            double exponent = -(x * x) / (2 * (sigma * sigma));
+
+            return coefficient * Math.Exp(exponent);
+        }
+
+        public static double GaussianCal(double sigma, int x, int y)
+        {
+            if (x < 0)
+            {
+                x = -x;
+            }
+            if (y < 0)
+            {
+                y = -y;
+            }
+
+            double coefficient = 1.0 / (2 * Math.PI * sigma * sigma);
+            double exponent = -((x * x) + (y * y)) / (2 * sigma * sigma);
+
+            return coefficient * Math.Exp(exponent);
+        }
+    }
+}
---
+++ Lab11/Lab11/FrequencyTable.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab11
+{
+    public static class FrequencyTable
+    {
+        public static List<Tuple<Tuple<int, int>, int>> GetFrequencyTable(int[] data, int maxBinCount)
+        {
+            int minNum = data[0];
+            int maxNum = data[0];
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                if (data[i] < minNum)
+                {
+                    minNum = data[i];
+                }
+                if (data[i] > maxNum)
+                {
+                    maxNum = data[i];
+                }
+            }
+
+            int intervalWidth = (maxNum - minNum) / maxBinCount + 1;
+
+            int binCount = (maxNum - minNum) / intervalWidth + 1;
+
+            var frequencyTable = new List<Tuple<Tuple<int, int>, int>>(binCount);
+
+            int coverage = minNum;
+
+            for (int i = 0; i < binCount; i++)
+            {
+                int count = 0;
+
+                for (int j = 0; j < data.Length; j++)
+                {
+                    if (data[j] >= coverage && data[j] < coverage + intervalWidth)
+                    {
+                        count++;
+                    }
+                }
+                var interval = new Tuple<int, int>(coverage, coverage + intervalWidth);
+                var frequency = new Tuple<Tuple<int, int>, int>(interval, count);
+
+                coverage = coverage + intervalWidth;
+
+                frequencyTable.Add(frequency);
+            }
+
+            return frequencyTable;
+        }
+    }
+}
